0fb7a98a3e4f57e8a007c072f1109c80b5c940b7
filipkgsson <filip.kgsson@gmail.com>
2018-11-23 14:36:26 +0100

    Added support for changing privilege and ban other users

--- GIT/Project1/src/Server.java
+++ GIT/Project1/src/Server.java
@@ -5,38 +5,47 @@ import java.io.*;
 public class Server {
 	public static final int PORT = 5678;
 	private static List<Connection> connectionList = new ArrayList<Connection>();
-	
+
 	public void updateClients(String msg) {
 		for (Connection connection : connectionList) {
 			connection.send(msg);
 		}
 	}
-	
+
 	public void updatePrivate(String msg, String name, String nameSender) {
-		for(Connection connection : connectionList) {
-			if(connection.name.equals(name) || connection.name.equals(nameSender)) {
+		for (Connection connection : connectionList) {
+			if (connection.name.equals(name) || connection.name.equals(nameSender)) {
 				connection.send(msg);
 			}
 		}
 	}
-	
+
 	public boolean checkName(String name) {
 		boolean result = false;
-		for(Connection connection : connectionList) {
-			if(connection.name.equals(name)) {
+		for (Connection connection : connectionList) {
+			if (connection.name.equals(name)) {
 				result = true;
 				break;
 			}
 		}
 		return result;
 	}
-	
+
 	public void addConnection(Connection c) {
 		connectionList.add(c);
 	}
-	
+
 	public void removeConnection(Connection c) {
 		connectionList.remove(c);
+	}
+
+	public void banUser(String name) {
+		for (Connection connection : connectionList) {
+			if (connection.name.equals(name)) {
+				connection.ban();
+				break;
+			}
+		}
 	}
 
 	public static void main(String args[]) {
@@ -50,8 +59,8 @@ public class Server {
 			}
 		} catch (IOException e) {
 			System.out.println("Listen :" + e.getMessage());
-		}finally {
-			if(listenSocket != null) {
+		} finally {
+			if (listenSocket != null) {
 				try {
 					listenSocket.close();
 				} catch (IOException e) {
@@ -68,6 +77,8 @@ class Connection extends Thread {
 	Socket clientSocket;
 	Server server;
 	String name;
+	String userType = "normal";
+	boolean banned = false;
 	private volatile boolean stop = false;
 
 	public Connection(Socket aClientSocket, Server s) {
@@ -80,7 +91,7 @@ class Connection extends Thread {
 			System.out.println("Connection:" + e.getMessage());
 		}
 	}
-	
+
 	public void send(String msg) {
 		try {
 			out.writeUTF(msg);
@@ -88,43 +99,57 @@ class Connection extends Thread {
 			System.out.println("Send: " + e.getMessage());
 		}
 	}
-	
+
+	public void ban() {
+		banned = true;
+	}
+
 	public void requestStop() {
 		stop = true;
 	}
 
 	public void run() {
-		while(!stop) {
+		while (!stop) {
 			try {
 				String message = in.readUTF();
-				if(message.startsWith("-setName")) {
-					String username = message.split(" ")[1];
-					if(server.checkName(username)) {
-						send("Error 777! Name is already taken");
-					}
-					else {
-						name = username;
-						server.addConnection(this);
-						send("Welcome " + username + "!");
-					}
-				}
-				else if(message.startsWith("-quit")) {
+				if (message.startsWith("-quit")) {
 					requestStop();
 					server.removeConnection(this);
 					server.updateClients(name + " disconnected from chat.");
 				}
-				else if(message.startsWith("@")) {
-					server.updatePrivate(name + ": " + message.substring(message.indexOf(" ")),
-							message.substring(1, message.indexOf(" ")), this.name);
-				}
-				else {
-					server.updateClients(name + ": " + message);
+				if (!banned) {
+					if (message.startsWith("-setName")) {
+						String username = message.split(" ")[1];
+						if (server.checkName(username)) {
+							send("Error 777! Name is already taken");
+						} else {
+							name = username;
+							server.addConnection(this);
+							send("Welcome " + username + "!");
+						}
+					} else if (message.startsWith("@")) {
+						server.updatePrivate(name + ": " + message.substring(message.indexOf(" ")),
+								message.substring(1, message.indexOf(" ")), this.name);
+					} else if (message.startsWith("-setPrivilege")) {
+						userType = message.split(" ")[1];
+						send("You are now a " + userType + " user!");
+					} else if (message.startsWith("-ban")) {
+						if (userType.equals("super")) {
+							String banName = message.split(" ")[1];
+							server.banUser(banName);
+							server.updateClients(banName + " has been banned from the chat by " + name);
+						} else {
+							send("You don't have permission to do that!");
+						}
+					} else {
+						server.updateClients(name + ": " + message);
+					}
 				}
 			} catch (EOFException e) {
 				System.out.println("EOF:" + e.getMessage());
 			} catch (IOException e) {
 				System.out.println("IO:" + e.getMessage());
-			} 
+			}
 		}
 	}
 }
