565bc0e20a5cbd02f1cb0561961fce96b89c0e36
filipkgsson <filip.kgsson@gmail.com>
2018-11-28 17:58:33 +0100

    Fixed the compareFiles method in SynchronisationServer.

--- GIT/Project3/src/SynchronisationClient.java
+++ GIT/Project3/src/SynchronisationClient.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
+import java.util.UUID;
 
 public class SynchronisationClient {
 	Socket s = null;
@@ -11,6 +13,13 @@ public class SynchronisationClient {
 			s = new Socket("localhost", SynchronisationServer.PORT);
 			in = new DataInputStream(s.getInputStream());
 			out = new DataOutputStream(s.getOutputStream());
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("What folder do you want to sync? ");
+			File directory = new File(scanner.nextLine());
+			scanner.close();
+			String id = UUID.randomUUID().toString();
+			out.writeUTF(id);
+			send(directory);
 		} catch (UnknownHostException e) {
 			System.out.println("Sock:" + e.getMessage());
 		} catch (EOFException e) {
@@ -23,13 +32,13 @@ public class SynchronisationClient {
 	public void send(File directory) {
 		FileInputStream fileStream = null;
 		try {
-			out.writeInt((int) directory.listFiles().length);
+			out.writeInt(directory.listFiles().length);
 			for (File file : directory.listFiles()) {
 				byte[] bytes = new byte[(int) file.length()];
 				fileStream = new FileInputStream(file);
 				fileStream.read(bytes);
 				out.writeUTF(file.getName());
-				out.writeInt((int) file.length());
+				out.writeInt(bytes.length);
 				out.write(bytes);
 			}
 		} catch (IOException e) {
@@ -49,34 +58,27 @@ public class SynchronisationClient {
 
 	public void receive() {
 		FileOutputStream fileStream = null;
-		File file = null;
 		try {
-			byte[] bytes = new byte[in.readInt()];
-			in.read(bytes);
-			file = new File("test");
-			fileStream = new FileOutputStream(file);
-			fileStream.write(bytes);
-//			File folder = new File("E:\\DSP\\Testmapp");
-//			File[] list = folder.listFiles();
-//			System.out.println(file.compareTo(list[0]));
-			// File file = File.createTempFile("test", ".txt");
+			int numberOfFiles = in.readInt();
+			File tempDirectory = new File("Temp");
+			tempDirectory.mkdir();
+			for(int i = 0; i < numberOfFiles; i++) {
+				File file = new File(tempDirectory.getName() + "\\" + in.readUTF());
+				fileStream = new FileOutputStream(file);
+				byte[] bytes = new byte[in.readInt()];
+				in.read(bytes);
+				fileStream.write(bytes);
+				fileStream.close();
+			}
+			System.out.println("The files that needs to be backed up is:");
+			for(File tempFile : tempDirectory.listFiles()) {
+				System.out.println(tempFile.getName());
+				tempFile.delete();
+			}
+			tempDirectory.delete();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally {
-
-			try {
-				if (fileStream != null) {
-					fileStream.close();
-				}
-				if (file != null) {
-					//file.delete();
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
 		}
 	}
 
@@ -84,9 +86,9 @@ public class SynchronisationClient {
 		SynchronisationClient c = new SynchronisationClient();
 		FileReceiver receiver = new FileReceiver(c);
 		receiver.start();
-		File folder = new File("Testmapp");
+		//File folder = new File("Testmapp");
 		// System.out.println(list[0].getName());
-		c.send(folder);
+		//c.send(folder);
 //		for (File file : list) {
 //			System.out.println(file.getName());
 //		}
--- GIT/Project3/src/SynchronisationServer.java
+++ GIT/Project3/src/SynchronisationServer.java
@@ -32,18 +32,32 @@ class Connection extends Thread {
 	DataOutputStream out;
 	Socket clientSocket;
 	ArrayList<File> directory = new ArrayList<File>();
+	String id;
 
 	public Connection(Socket aClientSocket) {
 		try {
 			clientSocket = aClientSocket;
 			in = new DataInputStream(clientSocket.getInputStream());
 			out = new DataOutputStream(clientSocket.getOutputStream());
+			id = in.readUTF();
+			File directory = new File(id);
+			directory.mkdir();
+			int numberOfFiles = in.readInt();
+			FileOutputStream fileOutStream = null;
+			for(int i = 0; i < numberOfFiles; i++) {
+				File file = new File(directory.getName() + "\\" + in.readUTF());
+				fileOutStream = new FileOutputStream(file);
+				byte[] bytes = new byte[in.readInt()];
+				in.read(bytes);
+				fileOutStream.write(bytes);
+				fileOutStream.close();
+			}
 		} catch (IOException e) {
 			System.out.println("Connection:" + e.getMessage());
 		}
 	}
 
-	public ArrayList<File> compareFiles(String id) {
+	public ArrayList<File> compareFiles() {
 		File fileDirectory = new File(id);
 		ArrayList<File> diff = new ArrayList<File>();
 		for (File tempFile : directory) {
@@ -57,7 +71,8 @@ class Connection extends Thread {
 					tempStream.read(tempBytes);
 					fileStream = new FileInputStream(file);
 					byte[] bytes = new byte[(int) file.length()];
-					if (tempBytes.equals(bytes)) {
+					fileStream.read(bytes);
+					if (compareBytes(tempBytes, bytes)) {
 						found = true;
 					}
 				} catch (IOException e) {
@@ -83,9 +98,24 @@ class Connection extends Thread {
 		}
 		return diff;
 	}
+	
+	public boolean compareBytes(byte[] a, byte[] b) {
+		if(a.length != b.length) {
+			return false;
+		}
+		else {
+			for(int i = 0; i < a.length; i++) {
+				if(a[i] != b[i]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 
 	public void run() {
 		FileOutputStream fileOutStream = null;
+		FileInputStream fileInStream = null;
 		try {
 			int numberOfFiles = in.readInt();
 			File tempDirectory = new File("ServerTemp");
@@ -99,11 +129,24 @@ class Connection extends Thread {
 				directory.add(file);
 				fileOutStream.close();
 			}
-			tempDirectory.delete();
-			ArrayList<File> diff = this.compareFiles("ServerTest");
+			ArrayList<File> diff = this.compareFiles();
+			out.writeInt(diff.size());
 			for(File file : diff) {
-				System.out.println(file.getName());
+				byte[] bytes = new byte[(int) file.length()];
+				fileInStream = new FileInputStream(file);
+				fileInStream.read(bytes);
+				out.writeUTF(file.getName());
+				out.writeInt(bytes.length);
+				out.write(bytes);
 			}
+			for(File tempFile : tempDirectory.listFiles()) {
+				tempFile.delete();
+			}
+			tempDirectory.delete();
+			directory.clear();
+//			for(File file : diff) {
+//				System.out.println(file.getName());
+//			}
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
